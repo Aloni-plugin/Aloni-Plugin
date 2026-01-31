ba0f18676914d494ecfff80a19f4695d956cab90
abdul-kh <Hywf\%22!qc]}5@U_P>
2026-01-31 20:57:38 +0100

    Hero-Section: Hero section implementation ready

--- src/sticky-cards-block/components/index.js
+++ src/sticky-cards-block/components/index.js
@@ -2,3 +2,4 @@ export { IntroSection } from './IntroSection';
 export { OutroSection } from './OutroSection';
 export { StickyCardsSection } from './StickyCardsSection';
 export { Navigation } from "./Navigation";
+export { DiagonalCutSection } from "./AloniDiagonalCutSection";
