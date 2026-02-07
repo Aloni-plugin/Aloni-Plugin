bab8e271f257384154349bdc031b2f894eda5aa9
Nizar Samman <nizar.samman42@gmail.com>
2026-02-07 22:10:54 +0100

    fixed sharedText

--- src/shared/text/SharedText.js
+++ src/shared/text/SharedText.js
@@ -70,48 +70,6 @@ export const SharedText = {
 				topText: "Kapsalon",
 				bottomText: "Deniz"
 			}
-		},
-		tailor: {
-			image: "/wp-content/plugins/Aloni-Plugin/images/kleding-reperatie-hero.png",
-			heading: "Klaar voor iets nieuws?",
-			primaryButton: {
-				text: "Neem contact op",
-				href: "/contact",
-				backgroundColor: "#F86060",
-				textColor: "#ffffff"
-			},
-			secondaryButton: {
-				text: "Onze prijslijst",
-				href: "/prijslijst",
-				backgroundColor: "transparent",
-				textColor: "#ffffff",
-				borderColor: "#ffffff"
-			},
-			logo: {
-				topText: "Atelier",
-				bottomText: "Deniz"
-			}
-		},
-		reviews: {
-			image: "/wp-content/plugins/Aloni-Plugin/images/kapsalon-homepage.png",
-			heading: "Wat onze klanten zeggen",
-			primaryButton: {
-				text: "Afspraak maken",
-				href: "/afspraak",
-				backgroundColor: "#F86060",
-				textColor: "#ffffff"
-			},
-			secondaryButton: {
-				text: "Onze prijslijst",
-				href: "/prijslijst",
-				backgroundColor: "transparent",
-				textColor: "#ffffff",
-				borderColor: "#ffffff"
-			},
-			logo: {
-				topText: "Kapsalon",
-				bottomText: "Deniz"
-			}
 		}
 	},
 	informationWithPhotos: {
